5a1e312d024d7e2ce46ec77e1242d38067fc1e72
chinhnt32 <chinhnt32@fpt.com>
2024-05-31 00:10:24 +0700

    test v2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send({'message': 'Hello World'})
+    res.send({'message': 'Hello World v2'})
 })
 
 var port = process.env.PORT || '8080';
